f25539d8c930d85b7a73f42bb1b71e87fbf698df
Jared Jones <jljones657@gmail.com>
2018-03-02 16:07:57 -0600

    CSS being attached to html file, and the the start of javascript file

---
+++ assets/javascript/app.js
@@ -0,0 +1,12 @@
+//Define variables
+$(document).ready(function(){
+    var time = 31;
+    var correct = 0
+    var incorrect = 0
+    var unanswered = 0
+
+// Questions and Answer Arrays
+    var question = ["What day is the longest possible day in the Northern Hemisphere, and the shortest possible day in the Southern Hemisphere?", "When was Pluto discovered?", "Who discovered Pluto?", "What is the largest of Saturn's Moons?", "How many known moons does Jupiter have?", "What is the largest known moon in our Solar System?", "What is the largest Planet in our Solar System?"]
+    var answer = ["Summer Solstice", "March 13th, 1930", "Clyde Tombaugh", "Titan", "69", "Ganymede", "Jupiter"]
+    
+}
